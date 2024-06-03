13e84ff287a32e14dded2d4cc6ede742d4cc2910
EduardaRospendowiski <eduarda2007casadooleiro@gmail.com>
2024-06-03 20:27:01 -0300

    Atividade concluídaaaaa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+double tempo;
+
+Console.WriteLine("--- Raio e trovão ---");
+
+ReceberValor();
+double converter = tempo * 340.29;
+ExibirValor();
+
+void ReceberValor()
+{
+    Console.Write("\nDigite o tempo em segundos entre o raio e o trovão: ");
+    tempo = Convert.ToDouble(Console.ReadLine());
+}
+
+void ExibirValor()
+{
+    Console.WriteLine($"\nA distância entre você e o raio é de: {converter / 1000:N5}km");
+}
